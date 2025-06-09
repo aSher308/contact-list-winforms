f895c9b3c0f296270ac2687c25e622963b1e27ef
Nguyntuanvu1203 <nguyntuanvu12@gmail.com>
2025-06-09 23:09:37 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ContactList
+{
+    partial class AddContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.btnAddContact = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(180, 95);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(100, 22);
+            this.txtHoTen.TabIndex = 0;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ Tên";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SDT";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(398, 213);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(100, 22);
+            this.txtSDT.TabIndex = 2;
+            // 
+            // btnAddContact
+            // 
+            this.btnAddContact.Location = new System.Drawing.Point(557, 133);
+            this.btnAddContact.Name = "btnAddContact";
+            this.btnAddContact.Size = new System.Drawing.Size(144, 23);
+            this.btnAddContact.TabIndex = 4;
+            this.btnAddContact.Text = "Thêm Liên Hệ";
+            this.btnAddContact.UseVisualStyleBackColor = true;
+            this.btnAddContact.Click += new System.EventHandler(this.btnAddContact_Click);
+            // 
+            // AddContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddContact);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHoTen);
+            this.Name = "AddContactForm";
+            this.Text = "AddContactForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Button btnAddContact;
+    }
+}
